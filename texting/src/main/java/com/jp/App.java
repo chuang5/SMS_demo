99a78e59051222caa62f4020cc73d2db5872c7c3
chuang5 <chen-feng.huang@sjsu.edu>
2021-05-29 15:04:27 +0800

    got data from json file

--- texting/src/main/java/com/jp/App.java
+++ texting/src/main/java/com/jp/App.java
@@ -1,21 +1,39 @@
 package com.jp;
 
+import java.io.BufferedReader;
+import java.io.Console;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import com.github.cliftonlabs.json_simple.JsonException;
+import com.github.cliftonlabs.json_simple.JsonObject;
+import com.github.cliftonlabs.json_simple.Jsoner;
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
 
-public final class App {
-    public static final String ACCOUNT_SID = System.getenv("TWILIO_ACCOUNT_SID");
-    public static final String AUTH_TOKEN = System.getenv("TWILIO_AUTH_TOKEN");
+public class App {
+    public static String ACCOUNT_SID;
+    public static String AUTH_TOKEN;
 
     public static void main(String[] args) {
-        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
-        Message message = Message.creator(
-                new PhoneNumber("+14157588772"),
-                new PhoneNumber("+12132385219"),
-                "This is a test message for java development.")
-            .create();
-
-        System.out.println(message.getSid());
+        try {
+            System.out.println(System.getProperty("user.dir"));
+            BufferedReader reader = Files.newBufferedReader(Paths.get("./texting/src/main/java/com/jp/user.json"));
+            JsonObject parser = (JsonObject) Jsoner.deserialize(reader);
+            ACCOUNT_SID = (String) parser.get("TWILIO_ACCOUNT_SID");
+            AUTH_TOKEN = (String) parser.get("TWILIO_AUTH_TOKEN");
+            Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+            Message message = Message.creator(
+                new PhoneNumber((String) parser.get("TO")), 
+                new PhoneNumber((String) parser.get("FROM")),
+                "Got data from json"
+            ).create();
+            System.out.println(message.getSid());
+        } catch (IOException | JsonException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 }
